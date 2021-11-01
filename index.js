d71416e84802263d287fb79d812ef8c6c4048fda
stranothus <stranothus@gmail.com>
2021-10-31 21:09:45 -0500

    Store alphabet

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
+const alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+String.prototype.map = function(func) {
+    return this.split("").map(func).join("");
+};
+
 function caesar(string, cipher) {
-    
+    return string.map(v => alphabet.indexOf(v) + 1 ? (alphabet[(alphabet.indexOf(v) + cipher) % alphabet.length])[v.toLowerCase() === v ? "toLowerCase" : "toUpperCase"]() : v);
+}
+
+String.prototype.caesar = function(cipher) {
+    return caesar(this, cipher);
+};
+
+Math.factorial = function(number) {
+    let toReturn = number;
+    while(number > 1) {
+        number--;
+        toReturn *= number;
+    }
+    return toReturn;
 }
