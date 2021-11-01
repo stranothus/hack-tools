23bf34cbecc19b03f803e0eea3178fa83a3b9541
stranothus <stranothus@gmail.com>
2021-11-01 10:34:05 -0500

    Get Selected Text

--- index.js
+++ index.js
@@ -20,3 +20,17 @@ Math.factorial = function(number) {
     }
     return toReturn;
 }
+
+function getSelectionText() {
+    let text = "";
+    if (window.getSelection) {
+        text = window.getSelection().toString();
+    } else if (document.selection && document.selection.type != "Control") {
+        text = document.selection.createRange().text;
+    }
+    return text;
+}
+
+window.addEventListener("contextmenu", event => {
+    console.log(getSelectionText());
+})
